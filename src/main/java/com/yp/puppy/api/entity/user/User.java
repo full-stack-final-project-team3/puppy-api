4dea4e751994c8a1939bb64672f57acf903d25eb
hgb926 <hgb926@naver.com>
2024-07-24 15:50:28 +0900

    0724 user 엔터티에 realName(실명) 필드 추가

--- src/main/java/com/yp/puppy/api/entity/user/User.java
+++ src/main/java/com/yp/puppy/api/entity/user/User.java
@@ -42,6 +42,8 @@ public class User {
     @Column(name = "email", nullable = false, unique = true)
     private String email;
 
+    private String realName;
+
     @Column(length = 500)
     private String password;
 
