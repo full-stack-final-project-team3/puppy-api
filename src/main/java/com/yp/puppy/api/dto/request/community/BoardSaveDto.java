9a9a184af2304da441ee413ea9baf19c2e92f248
KyoungkonCho <50573460+ckk914@users.noreply.github.com>
2024-08-05 10:14:17 +0900

    :pencil2: 게시글 등록 부분 수정(이미지 추가는 개선중)

--- src/main/java/com/yp/puppy/api/dto/request/community/BoardSaveDto.java
+++ src/main/java/com/yp/puppy/api/dto/request/community/BoardSaveDto.java
@@ -2,6 +2,7 @@ package com.yp.puppy.api.dto.request.community;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.yp.puppy.api.entity.community.Board;
+import com.yp.puppy.api.entity.user.User;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -13,19 +14,21 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class BoardSaveDto {
-    private String title;
-    private String content;
-    private String imageUrl;
+    private String boardTitle;
+    private String boardContent;
+    private String image;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime beginDate;
+    private LocalDateTime boardCreatedAt;
+    private User user;
 
     //엔터티로 변경하는 유틸 메서드
     public Board toEntity(){
         return Board.builder()
-                .boardTitle(this.title)
-                .boardContent(content)
-                .image(imageUrl)
-                .boardCreatedAt(beginDate)
+                .boardTitle(this.boardTitle)
+                .boardContent(boardContent)
+                .image(image)
+                .user(new User())
+                .boardCreatedAt( LocalDateTime.now())
                 .build();
     }
 }
