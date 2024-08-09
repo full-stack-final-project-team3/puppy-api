79fb7681cca3e480f9d7100aee952b03cd1874d7
kkimjuseong <rofodzl1@naver.com>
2024-08-09 12:24:58 +0900

    # 리뷰 작성할때 호텔이름 필요한거 추가

--- src/main/java/com/yp/puppy/api/dto/response/hotel/HotelReviewDetailDto.java
+++ src/main/java/com/yp/puppy/api/dto/response/hotel/HotelReviewDetailDto.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class HotelReviewDetailDto {p
+public class HotelReviewDetailDto {
 
     private String id;
     private String reviewContent;
