a9b3e2e0006c22c553adc3b99bc601567458dc3f
jzinit <kyj32323@naver.com>
2024-08-13 00:05:50 +0900

    StackOverflow 때문에 order 에 @JsonIgnore 추가

--- src/main/java/com/yp/puppy/api/entity/user/User.java
+++ src/main/java/com/yp/puppy/api/entity/user/User.java
@@ -123,6 +123,7 @@ public class User {
     @JsonIgnore
     private Cart cart;
 
+    @JsonIgnore //yj : StackOverflowError 때문에 추가했습니다
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Order> orders;
 
