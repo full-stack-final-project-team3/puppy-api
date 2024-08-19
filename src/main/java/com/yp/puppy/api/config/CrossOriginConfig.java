90337d3e99f03a4911a73843e9bcde8a947b1b7d
kkimjuseong <rofodzl1@naver.com>
2024-08-19 14:30:17 +0900

    # 웹 컨피그 삭제 크로스오리진 배포아이피 추가

--- src/main/java/com/yp/puppy/api/config/CrossOriginConfig.java
+++ src/main/java/com/yp/puppy/api/config/CrossOriginConfig.java
@@ -16,7 +16,7 @@ public class CrossOriginConfig implements WebMvcConfigurer {
             "http://localhost:3000",
             "http://localhost:3001",
             "http://localhost:3002",
-            "http://localhost:3003"
+            "http://3.34.195.72:3000"
     };
 
     @Override
