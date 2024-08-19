7609816a0b3fe3842fe9b96dee3f8cb6fc918710
hgb926 <hgb926@naver.com>
2024-08-19 23:14:02 +0900

    0819 인증메일 형식 수정

--- src/main/java/com/yp/puppy/api/service/user/UserService.java
+++ src/main/java/com/yp/puppy/api/service/user/UserService.java
@@ -148,13 +148,25 @@ public class UserService {
             messageHelper.setTo(email);
 
             // 이메일 제목 설정
-            messageHelper.setSubject("[Puppy verification email]");
+            messageHelper.setSubject("[Doggle 인증 코드입니다.]");
 
             // 이메일 내용
-            messageHelper.setText(
-                    "인증 코드: <b style=\"font-weight: 700; letter-spacing: 5px; font-size: 30px;\">" + code + "</b>"
-                    , true
-            );
+            String emailContent =
+                    "<div style=\"font-family: Arial, sans-serif; margin: 0 auto; width: 100%; max-width: 600px; padding: 20px; background-color: #f4f4f4;\">" +
+                            "<div style=\"background-color: #ffffff; padding: 40px; border-radius: 10px; text-align: center;\">" +
+                            "<h1 style=\"font-size: 24px; font-weight: bold; color: #333333;\">이메일 주소 인증 코드</h1>" +
+                            "<p style=\"font-size: 16px; color: #666666;\">안녕하세요.</p>" +
+                            "<p style=\"font-size: 16px; color: #666666;\">Doggle 서비스 이용을 위해 이메일 주소 인증을 요청하셨습니다.</p>" +
+                            "<p style=\"font-size: 16px; color: #666666;\">아래 코드를 입력해 인증을 완료하시면, 서비스를 이용하실 수 있습니다.</p>" +
+                            "<div style=\"margin: 20px 0; font-size: 22px; font-weight: bold; color: #333333;\">" +
+                            "인증 코드: <span style=\"font-weight: 700; letter-spacing: 5px; font-size: 30px; color: #14332C;\">" + code + "</span>" +
+                            "</div>" +
+//                            "</a>" +
+                            "<p style=\"font-size: 12px; color: #999999; margin-top: 20px;\">⚠ 인증은 5분 이내 완료해주세요.</p>" +
+                            "</div>" +
+                            "</div>";
+
+            messageHelper.setText(emailContent, true);
 
             // 전송자의 이메일 주소
             messageHelper.setFrom(mailHost);
