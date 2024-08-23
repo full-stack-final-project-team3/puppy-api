88d6ee33b521ab79f58837dd202eb12a113343ed
KyoungkonCho <50573460+ckk914@users.noreply.github.com>
2024-08-23 16:45:00 +0900

    :zap: 게시물 삭제 시퀀스 수정       id 조회수 제거 -> 게시물 제거하도록 수정

--- src/main/java/com/yp/puppy/api/repository/community/BoardViewRepository.java
+++ src/main/java/com/yp/puppy/api/repository/community/BoardViewRepository.java
@@ -9,4 +9,7 @@ import java.util.Optional;
 
 public interface BoardViewRepository extends JpaRepository<BoardView, Long> {
     Optional<BoardView> findByUserAndBoard(User user, Board board);
+
+    // 특정 Board와 연관된 모든 BoardView 엔티티들을 삭제하는 메서드
+    void deleteByBoard(Board board);
 }
